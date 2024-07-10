4571426bc898d8fbc134b9a1648757ec35d4b1a7
ncs <mrkhalnayak@xyz.com>
2024-07-10 19:05:55 +0530

    new commit

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -100,9 +100,35 @@ module "irsa-ebs-csi" {
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
-resource "aws_eks_addon" "vpc-addon" {
-  cluster_name                = var.cluster-name
-  addon_name                  = "vpc-cni"
-  addon_version               = "v1.18.2-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
-  resolve_conflicts_on_update = "PRESERVE"
+resource "aws_eks_addon" "vpc-cni" {
+  cluster_name  = module.eks.cluster_name
+  addon_name    = "vpc-cni"
+  addon_version = "v1.18.2-eksbuild.1"
+  configuration_values = jsonencode(
+    {
+      enableNetworkPolicy = "true"
+    }
+  )
+}
+
+resource "aws_eks_addon" "coreDNS" {
+  cluster_name = module.eks.cluster_name
+  addon_name = "coredns"
+  addon_version = "v1.11.1-eksbuild.9"
+  resolve_conflicts_on_create = "OVERWRITE"
+
+  tags = {
+    Name = "kube-proxy"
+  }
+
+}
+
+resource "aws_eks_addon" "kube-proxy" {
+  cluster_name = module.eks.cluster_name
+  addon_name = "kube-proxy"
+  addon_version = "v1.29.3-eksbuild.5"
+
+  tags = {
+    Name = "kube-proxy"
+  }
 }
